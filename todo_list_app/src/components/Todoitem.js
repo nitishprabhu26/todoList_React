9e7293aa442a70a94b05bb95a81bfe30c3b8a63c
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2020-10-30 16:44:19 -0500

    created a new component Todoitem.js

---
+++ todo_list_app/src/components/Todoitem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Todoitem extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello</h1>
+            </div>
+        )
+    }
+}
