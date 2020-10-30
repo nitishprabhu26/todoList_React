27770a69e56d3dc6b5d5eac1ae8bd14ccf22fb45
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2020-10-30 13:54:39 -0500

    created Todos component and imported it within app.js

--- todo_list_app/src/App.js
+++ todo_list_app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Todos from './components/Todos'
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Todos />
     </div>
   );
 }
---
+++ todo_list_app/src/components/Todos.js
@@ -0,0 +1,11 @@
+
+
+function Todos() {
+  return (
+    <div>
+      <h1>Todos</h1>
+    </div>
+  );
+}
+
+export default Todos;
