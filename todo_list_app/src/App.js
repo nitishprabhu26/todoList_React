d02b3d52633be9286430f4b81d9c5833061261f2
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2020-10-30 17:25:51 -0500

    Added basic Styles

--- todo_list_app/src/App.js
+++ todo_list_app/src/App.js
@@ -15,17 +15,17 @@ class App extends React.Component {
       {
         id: 1,
         title: "Title 1",
-        completed: "True"
+        completed: false
       },
       {
         id: 2,
         title: "Title 2",
-        completed: "False"
+        completed: true
       },
       {
         id: 3,
         title: "Title 3",
-        completed: "True"
+        completed: false
       }
     ]
   };
--- todo_list_app/src/components/Todoitem.js
+++ todo_list_app/src/components/Todoitem.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react'
 import PropTypes from "prop-types";
 
 export default class TodoItem extends Component {
+    // getStyle = () =>{
+    //     if(this.props.todo.completed){
+    //         return{
+    //             textDecoration: 'line-through'
+    //         }
+    //     } else{
+    //         return{
+    //             textDecoration: 'none'
+    //         }
+    //     }
+    // }
+
+    // or use Ternary operator:
+    getStyle = () =>{
+        return {
+            background: "#f4f4f4",
+            padding: "10px",
+            borderBottom: "1px #ccc dotted",
+            textDecoration: this.props.todo.completed ? "line-through" : "none",
+          };
+    }
+    
+
     render() {
         return (
-            <div>
-                <p>{this.props.todo.title}</p>
+            // <div style={itemStyle}></div>
+            // <div style={{backgroundColor:'#f4f4f4'}}>
+            //     <p>{this.props.todo.title}</p>
+            // </div>
+
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" />{" "}
+                    {this.props.todo.title}
+                </p>
             </div>
         )
     }
@@ -17,6 +48,10 @@ TodoItem.propTypes = {
     // markComplete: PropTypes.func.isRequired,
     // delTodo: PropTypes.func.isRequired,
   };
+
+//   const itemStyle = {
+//       backgroundColor:'#f4f4f4'
+//     }
 
 // not needed since check line 3
 // export default TodoItem
