d06ff55834f66dccb455cca2dac5077c8b1ed38a
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2020-10-30 15:44:01 -0500

    using map method to loop through array and outputting jsx

--- todo_list_app/src/components/Todos.js
+++ todo_list_app/src/components/Todos.js
@@ -6,11 +6,11 @@ class Todos extends React.Component {
 
 render(){
     // console.log(this.props.Todos)
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+
+    // https://reactjs.org/docs/lists-and-keys.html#keys
+  return this.props.todos.map((todo) => (
+  <h3 key={todo.id}>{ todo.title}</h3>
+  ));
 
 }
 }
