c7e1ff6e07b4cdbc49e9596ecbb639c4a8ffb64a
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2020-10-31 04:36:44 -0500

    Created AddTodo component

--- todo_list_app/src/App.js
+++ todo_list_app/src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
-import React from 'react';
-import Todos from './components/Todos'
-import Header from './components/layout/Header'
-
+import "./App.css";
+import React from "react";
+import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import { v4 as uuid } from "uuid";
 
 // or
 // ientationimport React, { Component } from 'react'
@@ -10,60 +11,75 @@ import Header from './components/layout/Header'
 // }
 
 class App extends React.Component {
-
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid(),
         title: "Submit Assignment on Time",
-        completed: false
+        completed: false,
       },
       {
-        id: 2,
+        id: uuid(),
         title: "Prepare for the Test",
-        completed: true
+        completed: true,
       },
       {
-        id: 3,
+        id: uuid(),
         title: "Pay the bills",
-        completed: false
-      }
-    ]
+        completed: false,
+      },
+    ],
   };
 
+  // Toggle Complete
+  markComplete = (id) => {
+    // console.log(id)
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
 
-    // Toggle Complete
-    markComplete = id => {
-      // console.log(id)
-      this.setState({
-        todos: this.state.todos.map(todo => {
-          if (todo.id === id) {
-            todo.completed = !todo.completed;
-          }
-          return todo;
-        })
-      });
-    };
+  // Delete Todo
+  // copying everything which is already there -> using spread operator (...)
+  delTodo = (id) => {
+    // console.log(id)
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
 
-    // Delete Todo
-    // copying everything which is already there -> using spread operator (...)
-    delTodo = id => {
-      console.log(id)
-      this.setState({
-        todos: [...this.state.todos.filter(todo => todo.id !== id)]
-      })
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title: title,
+      completed: false,
     };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
 
   render() {
     // console.log(this.state.todos)
     return (
       <div className="App">
-        <Header />
-        {/* Taking Todos in state , passing it to Todos component as props */}
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          {/* Taking Todos in state , passing it to Todos component as props */}
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
-}
+  }
 }
 
 export default App;
